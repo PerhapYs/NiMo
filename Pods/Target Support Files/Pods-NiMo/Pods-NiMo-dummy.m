73db020935a4367074cf7d2a8d0c7ee9298d346e
370060080@qq.com <370060080@qq.com>
2017-05-04 12:56:59 +0800

    基本配置

---
+++ Pods/Target Support Files/Pods-NiMo/Pods-NiMo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NiMo : NSObject
+@end
+@implementation PodsDummy_Pods_NiMo
+@end
---
+++ Pods/Target Support Files/Pods-NiMoTests/Pods-NiMoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NiMoTests : NSObject
+@end
+@implementation PodsDummy_Pods_NiMoTests
+@end
